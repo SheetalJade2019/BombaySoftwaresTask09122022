42129a60bd36cef70e9332c2d3f16eebed7a7c3d
SheetalJade2019 <sheetal.jade93@gmail.com>
2022-12-09 16:41:34 +0530

    first

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+ 
+class User(AbstractUser):
+    email = models.EmailField(max_length=250,unique=True)
+    first_name = models.CharField(max_length=80, default=0)
+    last_name = models.CharField(max_length=80, default=0)
+    User_ID = models.AutoField(primary_key=True)
+    address = models.CharField(max_length=250, blank=True,null=True)
+    session_token = models.CharField(max_length=20, default=0)
+    phone = models.CharField(unique=True,max_length=20)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    Dob = models.DateTimeField(null=True)
+    gender = models.CharField(max_length=10, default=0)
+    Type = models.CharField(max_length=10, default=0)
+    password = models.CharField(max_length=100,null=True,blank=True)
+    # standard
---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    pass
+
+def register_user(request):
+    pass
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
+
+
+def dashboard(request):
+    pass
