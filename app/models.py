daf71f49afd3297a1757baa4c90520e61f28dd2f
SheetalJade2019 <sheetal.jade93@gmail.com>
2022-12-09 16:55:26 +0530

    models.py modified

--- app/models.py
+++ app/models.py
@@ -5,6 +5,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
  
+
+user_gender_choices = [('Male','Male'),('Female','Female'),('Other','Other')]
+user_type_choices = [('Primary','Primary'),('Secondary','Secondary')]
+
 class User(AbstractUser):
     email = models.EmailField(max_length=250,unique=True)
     first_name = models.CharField(max_length=80, default=0)
@@ -17,7 +21,7 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     Dob = models.DateTimeField(null=True)
-    gender = models.CharField(max_length=10, default=0)
-    Type = models.CharField(max_length=10, default=0)
+    gender = models.CharField(max_length=10,choices=user_gender_choices default=0)
+    Type = models.CharField(max_length=10,choices=user_type_choices default=0)
     password = models.CharField(max_length=100,null=True,blank=True)
     # standard
